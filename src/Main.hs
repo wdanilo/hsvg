150967edf6ebaf8d152119f044f1166f76a9fc19
Wojciech Danilo <wojciech.danilo@gmail.com>
2017-11-20 15:01:04 +0100

    Overall progress

--- src/Main.hs
+++ src/Main.hs
@@ -14,6 +14,9 @@ import qualified Data.Text.IO as Text
 import qualified Data.Matrix  as Matrix
 import           Data.Matrix  (Matrix(..))
 
+import qualified Data.Color as Color
+import           Data.Fixed              (mod')
+import           Data.Hashable (Hashable, hash)
 
 -----------------------
 -- === Transform === --
@@ -48,11 +51,19 @@ data Boolean a
 
 
 
+-----------------
+-- === Dim === --
+-----------------
+
+class           Dim1 a where x :: Lens' a Double
+class Dim1 a => Dim2 a where y :: Lens' a Double
+class Dim2 a => Dim3 a where z :: Lens' a Double
+
 --------------------
 -- === Points === --
 --------------------
 
-data Point = Point { _x :: Double, _y :: Double } deriving (Eq, Ord, Show)
+data Point = Point { __x :: Double, __y :: Double } deriving (Eq, Ord, Show)
 data ControlPoint = ControlPoint
     { _leftHandle  :: Maybe Point
     , _rightHandle :: Maybe Point
@@ -68,6 +79,8 @@ instance (a~b, a~Double) => Convertible (a,b) Point where
 instance (a~b, a~Double) => Convertible (a,b) ControlPoint where
   convert = ControlPoint mempty mempty . convert
 
+instance Dim1 Point where x = point_x
+instance Dim2 Point where y = point_y
 
 pt   ::                     Double -> Double                     -> ControlPoint
 lpt  :: Double -> Double -> Double -> Double                     -> ControlPoint
@@ -77,6 +90,50 @@ pt         x y       = ControlPoint Nothing              Nothing              $ 
 lpt  lx ly x y       = ControlPoint (Just $ Point lx ly) Nothing              $ Point x y
 rpt        x y rx ry = ControlPoint Nothing              (Just $ Point rx ry) $ Point x y
 lrpt lx ly x y rx ry = ControlPoint (Just $ Point lx ly) (Just $ Point rx ry) $ Point x y
+
+
+
+---------------------
+-- === Vectors === --
+---------------------
+
+data Vector = Vector { __x :: Double, __y :: Double }
+makeLenses ''Vector
+
+len :: Vector -> Double
+len v = sqrt $ (v ^. x) * (v ^. x) + (v ^. y) * (v ^. y)
+
+sub :: Point -> Point -> Vector
+sub (Point x y) (Point x' y') = Vector (x - x') (y - y')
+
+mul :: Double -> Vector -> Vector
+mul a (Vector x y) = Vector (x * a) (y * a)
+
+dv :: Double -> Vector -> Vector
+dv a (Vector x y) = Vector (x / a) (y / a)
+
+unit :: Vector -> Vector
+unit v = dv (len v) v
+
+txPt :: Vector -> Point -> Point
+txPt (Vector x y) (Point x' y') = Point (x + x') (y + y')
+
+instance Dim1 Vector where x = vector_x
+instance Dim2 Vector where y = vector_y
+
+instance Num Vector where
+    Vector x y + Vector x' y' = Vector (x + x') (y + y')
+    Vector x y - Vector x' y' = Vector (x - x') (y - y')
+
+
+---------------------------
+-- === Smooth Points === --
+---------------------------
+
+data SmoothPoint = SmoothPoint { _point :: Point, _smooth :: Double } deriving (Eq, Ord, Show)
+
+spt :: Double -> Double -> Double -> SmoothPoint
+spt x y s = SmoothPoint (Point x y) s
 
 
 ----------------------
@@ -96,7 +153,9 @@ data GeoDef
 data Shape
   = Rect   Double Double
   | Circle Double
+  | Arc    Double Double
   | Path   [ControlPoint]
+  | SPath  [SmoothPoint]
   deriving (Eq, Ord, Show)
 
 data Hinting
@@ -123,15 +182,39 @@ makeLenses ''Geo
 rect :: Double -> Double -> Geo
 rect = Geo mempty mempty . GeoSimple .: Rect
 
+-- FIXME: rect' should logically replace rect
+rect' :: Double -> Double -> Geo
+rect' w h = move (-w/2) (-h/2) $ rect w h
+
 square :: Double -> Geo
 square a = rect a a
 
 circle :: Double -> Geo
 circle = Geo mempty mempty . GeoSimple . Circle
 
+arc :: Double -> Double -> Geo
+arc x y = Geo mempty mempty . GeoSimple $ Arc x y
+
 path :: Convertible' p ControlPoint => [p] -> Geo
 path = Geo mempty mempty . GeoSimple . Path . fmap convert'
 
+spath :: Convertible' p SmoothPoint => [p] -> Geo
+spath = Geo mempty mempty . GeoSimple . SPath . fmap convert'
+
+spath2Path :: [SmoothPoint] -> [ControlPoint]
+spath2Path = \case
+    []  -> []
+    lst -> spath2Path' (unsafeLast lst : lst <> [unsafeHead lst])
+
+-- FIXME: TO FINISH
+spath2Path' :: [SmoothPoint] -> [ControlPoint]
+spath2Path' = undefined -- \case
+    -- (SmoothPoint prevPt _ : current@(SmoothPoint pt s rs) : next@(SmoothPoint nextPt _) : rest) -> ControlPoint lh rh pt : spath2Path' (current : next : rest) where
+    --     npts = if s == 0 then [ControlPoint Nothing Nothing pt]
+    --                      else [ControlPoint Nothing (Just pt) $ txPt (mul s . unit $ sub prevPt pt) pt
+    --     -- lh = Nothing
+    --     -- rh = Nothing
+    -- _ -> []
 
 -- === Styling === --
 
@@ -302,9 +385,15 @@ renderGeoDef rest g = case g of
       registerMask idb
       return $ "<use href=\"#s" <> show' ida <> "\"" <> " mask=" <> quoted ("url(#m" <> show' idb <> ")") <> rest <> "/>"
 
+-- M0 0 A 100 100 0 0 0 100 100 L 100 0
   GeoSimple s -> case s of
     Rect w h -> return $ "<rect width=" <> quoted (show' w) <> " height=" <> quoted(show' h) <> rest <> "/>"
     Circle r -> return $ "<circle r=" <> quoted (show' r) <> rest <> "/>"
+    Arc r a  -> return $ "<path d=" <> quoted ("M" <> show' (-r) <> " 0 A " <> show' r <> " " <> show' r <> " 0 " <> show' flag <> " 0 " <> show' nx <> " " <> show' ny <> "L 0 0 Z") <> rest <> "/>" where
+        nx = - r * cos a
+        ny = r * sin a
+        flag = if a > pi then 1 else 0
+    SPath pts -> renderGeoDef rest (GeoSimple . Path $ spath2Path pts)
     Path pts -> return $ "<path d=" <> quoted body <> rest <> "/>" where
       body = case pts of
         (p:ps) -> "M" <> show' (p ^. point.x) <> "," <> show' (p ^. point.y) <> " "
@@ -324,8 +413,8 @@ renderGeoDef rest g = case g of
         [] -> []
         s  -> unsafeLast s : unsafeInit s
 
-_w = 200
-_h = 200
+_w = 2880
+_h = 1800
 
 fullBg = fill "white" $ move (-_w) (-_h) $ rect (2*_w) (2*_h)
 
@@ -388,11 +477,114 @@ databaseIcon = seg baseColor 10 0 + move 0 32 (seg baseColor2 0 0) + move 0 64 (
   front        = move 18 15 (fill bgColor ball)
                + move 50 10 (fill bgColor dots)
 
+panelColor = "#161514"
+tabHeight = 60
+
+-- 2880 x 1600
+navPanel :: Geo
+navPanel = fill panelColor $ rect 400 _h
+
+graphPanel :: Geo
+graphPanel = fill panelColor $ bg + tb where
+    bg = move 0 tabHeight $ rect 2480 (_h - tabHeight)
+    tb = tab 300
+
+vsep :: Double -> Geo
+vsep x = move x 0 $ mconcat
+         [ fill "#000000" (rect 4 _h)
+         , fill "rgba(255,255,255,0.03)" (move 4 0 $ rect 2 _h)
+         ]
+
+tabBar :: Double -> Geo
+tabBar w = fill "#000000" $ rect w 60
+
+tab :: Double -> Geo
+tab w = fill panelColor $ path [pt 0 0, rpt w 0 (w + r) 0, lpt (w + slope - r) tabHeight (w + slope) tabHeight, pt 0 tabHeight] where
+    r     = 70
+    slope = 100
+
+
+compactNode :: [String] -> [String] -> Geo
+compactNode ins outs = ports ins + rotate 180 (ports outs) where
+    portShape s = rotate (90) $ arc rad (pi / s) - circle (rad - width) - span - rotateRad (-pi / s) span
+    port n s   = fill ("rgb(" <> convert (show $ round r) <> "," <> convert (show $ round g) <> "," <> convert (show $ round b) <> ")") $ portShape s where
+        Color.RGB r g b _ = Color.lch2rgb $ buildLCH (Color $ hash n)
+    ports ns  = mconcat $ (\(n,i) -> rotate (-i * 180 / num) $ port n num) <$> zip ns [0 .. num - 1] where num = convert (length ns)
+    span    = rect' (2 * rad + 10) off
+    width   = 8
+    rad     = 50
+    off     = 4
+
+arrow :: Point -> Point -> Geo
+arrow p p' = rotateRad (angle p p') $ linkLine len + (move len 0 arrowHead) where
+    len    = dist p p'
+
+arrowHead :: Geo
+arrowHead = path [pt 0 headW, pt headL 0, pt 0 (-headW)] where
+    headW  = 8
+    headL  = 10
+
+link :: Point -> Point -> Geo
+link p p' = rotateRad (angle p p') $ linkLine len + move (len / 2) 0 arrowHead where 
+    len   = dist p p'
+
+linkLine :: Double -> Geo
+linkLine len = move 0 (-aWidth/2) $ rect len aWidth where
+    aWidth = 4
+
+dist :: Point -> Point -> Double
+dist (Point sx sy) (Point ex ey) = sqrt $ dx * dx + dy * dy where
+    dx = ex - sx
+    dy = ey - sy
+
+angle :: Point -> Point -> Double
+angle (Point sx sy) (Point ex ey) = atan2 (ey - sy) (ex - sx)
+
+
+-- graph :: Map Int
+
+
+data NodeType = CompactNode | NormalNode deriving (Show)
+
+data Node = Node { _pos     :: Point
+                 , _inputs  :: [String]
+                 , _outputs :: [String]
+                 , _panels  :: NodeType
+                 }
+
+renderNode :: Node -> Geo
+renderNode (Node pos ins outs pans) = move (pos ^. x) (pos ^. y) $ case pans of
+    CompactNode -> compactNode ins outs
+
+
+newtype Color = Color { fromColor :: Int } deriving (Eq, Generic, Ord, Show)
+
+colorC :: Int -> Float
+colorC _ = 45
+
+colorL :: Int -> Float
+colorL _ = 30
+
+colorH :: Int -> Int
+colorH _ = 100
+
+buildLCH :: Color -> Color.LCH
+buildLCH (Color 0) = Color.LCH 50  0 0 255
+buildLCH (Color i) = Color.LCH (colorL i) (colorC i) (convert h') 255 where
+    h'    = (colorH i + (i - 1)) `mod'` 360
+
+
 main :: IO ()
 main = do
-  let svg = render _w _h $ move 12 12 databaseIcon
+  let nodes = [ Node (Point 600 600) ["Int", "String", "Vector"] ["Int"] CompactNode
+              , Node (Point 800 600) ["Int"] ["Int"] CompactNode
+              ]
+  let svg = render _w _h
+          $ mconcat [navPanel, move 400 0 graphPanel, vsep 400
+                    , fill "#ff0000" $ move 650 600 $ link (Point 0 0) (Point 100 0)
+                    ] <> mconcat (renderNode <$> nodes)
 
-
+  print $ Color.lch2rgb $ buildLCH (Color 7)
   print svg
   Text.writeFile "/tmp/test.svg"  svg
   Text.writeFile "/tmp/test.html" (mkView svg)
@@ -401,6 +593,6 @@ main = do
 
 mkView s = "<!DOCTYPE html>"
         <> "<html>"
-        <> "<body id=\"body\" style=\"zoom:1; margin:0; background-color:#141210\">"
+        <> "<body id=\"body\" style=\"zoom:1; margin:0; background-color:#000000\">"
         <> s
         <> "</body></html>"
